e4d2f8080cc91b8911833355818268445385b89f
Skullman-G <skullmanslashgames@gmail.com>
2026-03-13 08:32:59 +0100

    use same three version as before

--- games/turborace/scripts/three.js
+++ games/turborace/scripts/three.js
@@ -1,1 +1,1 @@
-export * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+export * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js';
